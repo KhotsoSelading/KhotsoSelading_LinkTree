0c80c780ea95e1cd3691c538a49e747d84b35ac0
KhotsoSelading <k.selading007@gmail.com>
2024-03-26 07:59:12 +0200

    Redirect Data

---
+++ data.js
@@ -0,0 +1,57 @@
+export const creatorDetails = {
+  accountHandle: "@KSelading",
+  creatorName: "Khotso Selading",
+  tagline: "Connect with me",
+  profilePic: "profile-pic.jpg",
+};
+
+export const socialLinks = [
+  {
+    url: "https://www.tiktok.com/",
+    iconName: "tiktok",
+    active: false,
+  },
+  {
+    url: "https://www.youtube.com",
+    iconName: "youtube",
+    active: true,
+  },
+  {
+    url: "https://twitter.com/",
+    iconName: "twitter",
+    active: true,
+  },
+  {
+    url: "https://github.com/",
+    iconName: "github",
+    active: true,
+  },
+  {
+    url: "https://www.linkedin.com/",
+    iconName: "linkedin",
+    active: true,
+  },
+  {
+    url: "https://www.instagram.com/",
+    iconName: "instagram",
+    active: true,
+  },
+];
+
+export const whereNextLinks = [
+  {
+    url: "https://khotso-personal-profile.vercel.app/",
+    text: "My developer portfolio",
+    active: true,
+  },
+  {
+    url: "https://hearthis.at/ravi-bling/",
+    text: "My Dj portfolio",
+    active: true,
+  },
+  {
+    url: "https://www.youtube.com/",
+    text: "How to become a web developer",
+    active: true,
+  },
+];
